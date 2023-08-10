0ff24f6819f039719388d5aafb238e8428fba159
Javi <javierllinaresbarral@outlook.com>
2023-08-10 11:00:05 +0200

    Añadidas nuevos estilos, finalizar header

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const {src, dest, watch} = require("gulp") //Este gulp es el que instalamos y vemos en el package.json y el require es una forma de extraerlo
 //src sirve para identificar un archivo y dest para guardarlo
 const sass = require("gulp-sass")(require("sass"))
+const plumber = require("gulp-plumber")
 
 function css(done) {
   src("src/scss/**/*.scss") //Identificar el archivo de SASS
+    .pipe(plumber())
     .pipe(sass()) //Compilarlo, ejecutar las funciones de SASS
     .pipe(dest("build/css")) //Guardarlo en el disco duro
 
