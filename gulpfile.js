ef1b2d6b02ccb7a1b6046661761bf49952cd469d
Javi <javierllinaresbarral@outlook.com>
2023-08-16 10:49:43 +0200

    Añadido scroll y optimizacion del proyecto con Terser, CSS Nano y PostCSS

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ const {src, dest, watch, parallel} = require("gulp")
 //CSS
 const sass = require("gulp-sass")(require("sass"))
 const plumber = require("gulp-plumber")
+const autoprefixer = require("autoprefixer")
+const cssnano = require("cssnano")
+const postcss = require("gulp-postcss")
+const sourcemaps = require("gulp-sourcemaps")
 
 //Imágenes
 const cache = require("gulp-cache")
@@ -12,10 +16,16 @@ const imagemin = require("gulp-imagemin")
 const webp = require("gulp-webp")
 const avif = require("gulp-avif")
 
+//Javascript
+const terser = require("gulp-terser-js")
+
 function css(done) {
   src("src/scss/**/*.scss") //Identificar el archivo de SASS
+    .pipe(sourcemaps.init())
     .pipe(plumber())
     .pipe(sass()) //Compilarlo, ejecutar las funciones de SASS
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write("."))
     .pipe(dest("build/css")) //Guardarlo en el disco duro
 
   done() //Callback que avisa a gulp cuando llegamos al final
@@ -48,7 +58,11 @@ function versionAvif(done) {
 }
 
 function javascript(done) {
-  src("src/js/**/*.js").pipe(dest("build/js"))
+  src("src/js/**/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(terser())
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("build/js"))
   done()
 }
 
